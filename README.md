bcfa89b62fe558324a6c09c43b445532f7eab758
nurbelawahyuningse2327240105-ux <nurbelawahyuningse_2327240105@mhs.mdp.ac.id>
2025-10-23 14:50:47 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# wisata_candi_nbela
+
---
+++ wisata_candi_nurbela-main/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:wisata_candi_nurbela/data/candi_data.dart';
+import 'screens/detail_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wisata Candi',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: DetailScreen(candi: candiList[0]),
+    );
+  }
+}
+
+
+
+
+
