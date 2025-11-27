6915be6256b84ff587d87d07998ae853c8b4c7d3
nurbelawahyuningse2327240105-ux <nurbelawahyuningse_2327240105@mhs.mdp.ac.id>
2025-11-27 17:00:51 +0700

    fav update

--- lib/screens/favorit_screen.dart
+++ lib/screens/favorit_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import '../data/candi_data.dart';
+import '../models/candi.dart';
+import '../widgets/item_card.dart';
 
 class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
@@ -8,11 +11,21 @@ class FavoriteScreen extends StatefulWidget {
 }
 
 class _FavoriteScreenState extends State<FavoriteScreen> {
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-
+    return Scaffold(
+      appBar: AppBar(title: Text('Favorite')),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        padding: EdgeInsets.all(8),
+        itemCount: candiList.length,
+        itemBuilder: (context, index) {
+          Candi candi = candiList[index];
+          return ItemCard(candi: candi);
+        },
+      ),
     );
   }
 }
--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-
-import '../models/candi.dart';
-=======
 import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/models/candi.dart';
->>>>>>> 0a63f306185f8b1076cee1b67213018922c0648a
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -15,115 +10,54 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-<<<<<<< HEAD
-  // TODO: 1. Deklarasikan variabel yang dibutuhkan
-  List<Candi> _filteredCandis = [];
-  String _searchQuery = '';
-  final TextEditingController _searchController = TextEditingController();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      //TODO: 2. Buat appbar dengan judul pencarian candi
-      appBar: AppBar(title: Text('pencarian Candi'),),
-      //TODO: 3. Buat body berupa column
-      body: Column(
-        children: [
-          //TODO: 4. Buat TextField pencarian sebagai anak dari column
-          Padding(
-            padding: EdgeInsets.all(16),
-            child : Container(
-=======
-//   TODO: 1. Deklarasi variabel yang dibutuhkan
-
+  //TODO 1 Variabel
   List<Candi> _filteredCandis = candiList;
-  String _searchQuery = '';
+  String _searchQuery = "";
   final TextEditingController _searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //   TODO: 2. Buat appbar dengan judul pencarian candi
-      appBar: AppBar(title: Text('Pencarian Candi'),),
-      //   TODO: 3. Buat body berupa column
+      //TODO 2 APPBar
+      appBar: AppBar(
+        title: Text('Pencarian Candi'),
+      ),
+      //TODO 3 Body
       body: Column(
         children: [
-          //   TODO: 4. Buat TextField pencarian sebagai anak dari column
+          //TODO 4 Textfield Search
           Padding(
             padding: EdgeInsets.all(16),
             child: Container(
->>>>>>> 0a63f306185f8b1076cee1b67213018922c0648a
               decoration: BoxDecoration(
-                color: Colors.deepPurple[50],
-                borderRadius: BorderRadius.circular(8),
-              ),
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.deepPurple[50]),
               child: TextField(
-                controller: _searchController,
-<<<<<<< HEAD
-                onChanged: (value){
-=======
-                onChanged: (value) {
->>>>>>> 0a63f306185f8b1076cee1b67213018922c0648a
-                  setState(() {
-                    _searchQuery = value;
-                  });
-                },
-<<<<<<< HEAD
-                //TODO: 6. Implement5s fitur pencarian
+                autofocus: false,
                 decoration: InputDecoration(
-                  hintText: 'Cari Candi....',
-                    prefixIcon: Icon(
-                      Icons.search,
-                      color: Colors.deepPurple
-
-                    ),
-                  //TODO: 7. implementasi pengosongan input
-                  border: InputBorder.none,
-                  contentPadding:
-                    EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                ),
-              ),
-            )
-          )
-          //TODO: 5. buat ListView hasil pencarian sebagai anak dari colum
-          Expanded(
-            child: ListView.builder(
-              itemCount: _filteredCandis.length,
-              itemBuilder: (context, index) {
-
-              },
-            ),
-          )
-
-=======
-                // TODO: 6. Implementasi fitur pencarian
-                decoration: InputDecoration(
-                  hintText: 'Cari candi...',
-                  prefixIcon: Icon(
-                      Icons.search,
-                      color: Colors.deepPurple
-                  ),
-                  // TODO 7. Implementasi pengosongan input
-                  border: InputBorder.none,
-                  contentPadding:
-                  EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                ),
+                    hintText: "Cari Candi....",
+                    prefixIcon: Icon(Icons.search),
+                    border: InputBorder.none,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.deepPurple)),
+                    contentPadding:
+                    EdgeInsets.symmetric(horizontal: 16, vertical: 12)),
               ),
             ),
           ),
-          //   TODO: 5. Buat ListView hasil pencarian sebagai anak dari Column
+          //TODO 5 Listview
           Expanded(
             child: ListView.builder(
                 itemCount: _filteredCandis.length,
-                itemBuilder: (context, index) {
+                itemBuilder: (content, index) {
                   final candi = _filteredCandis[index];
-                  // TODO: 8. Implementasi GestureDetector dan Hero animation
                   return Card(
                     margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
                     child: Row(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Container(
-                          padding: EdgeInsets.all(8),
+                          padding: const EdgeInsets.all(8),
                           width: 100,
                           height: 100,
                           child: ClipRRect(
@@ -135,42 +69,31 @@ class _SearchScreenState extends State<SearchScreen> {
                           ),
                         ),
                         Padding(
-                          padding: EdgeInsets.all(8),
+                          padding: const EdgeInsets.all(8.0),
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(
                                 candi.name,
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                                style: TextStyle(fontSize: 16),
                               ),
-                              SizedBox(height: 4),
-                              Text(
-                                candi.location,
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                              SizedBox(
+                                height: 4,
                               ),
+                              Text(candi.location)
                             ],
                           ),
-                        ),
+                        )
                       ],
                     ),
                   );
-                }
-            ),
+                }),
           ),
->>>>>>> 0a63f306185f8b1076cee1b67213018922c0648a
+          const SizedBox(
+            height: 16,
+          )
         ],
       ),
     );
   }
-<<<<<<< HEAD
 }
-
-=======
-}
->>>>>>> 0a63f306185f8b1076cee1b67213018922c0648a
